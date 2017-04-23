26867c430042a8f2f0f5402cdd2768e63e1cfb51
stef-dhaeseleer <stefdhaeseleer@gmail.com>
2017-04-23 11:06:37 +0200

    Chapter 2 finished

--- texty.c
+++ texty.c
@@ -1,3 +1,62 @@
+/*** includes ***/
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** data ***/
+
+struct termios orig_termios;    // Saving the original terminal attributes.
+
+/*** terminal ***/
+
+// Error message printing and exiting
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
+// Setting the original terminal attributes back.
+void disableRawMode() {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+}
+
+// Function to edit the Terminal attributes
+void enableRawMode() {
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    atexit(disableRawMode);     // Set back original at end main() or exit()
+    
+    struct termios raw = orig_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+}
+
+/*** init ***/
+
 int main() {
+    enableRawMode();
+    
+    char c;
+    // read() returns the amount of read bytes, we store the read characters in the char c. We also exit upon the 'q'.
+    while (1) {
+        char c = '\0';
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
+        if (iscntrl(c)) {   // Checks for control characters like TAB
+            printf("%d\r\n", c);    // Carriage return \r and newline \n
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+        if (c == 'q') break;
+    }
     return 0;
 }
