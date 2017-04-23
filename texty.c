d7ab4b7aec8035e41b0b6c506c1a278de481797b
stef-dhaeseleer <stefdhaeseleer@gmail.com>
2017-04-23 12:56:32 +0200

    Completed up to hide the cursor when repainting

--- texty.c
+++ texty.c
@@ -4,6 +4,8 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -13,28 +15,38 @@
 
 /*** data ***/
 
-struct termios orig_termios;    // Saving the original terminal attributes.
+struct editorConfig {
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 /*** terminal ***/
 
 // Error message printing and exiting
 void die(const char *s) {
+    // Clear and reposition cursor
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    
     perror(s);
     exit(1);
 }
 
 // Setting the original terminal attributes back.
 void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
 // Function to edit the Terminal attributes
 void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);     // Set back original at end main() or exit()
     
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -45,21 +57,135 @@ void enableRawMode() {
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+// read() returns the amount of read bytes, we store the read characters in the char c. We also exit upon the 'q'.
+char editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+// Fallback method for getting the terminal size when ioctl() fails
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+    
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+    
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    
+    buf[i] = '\0';
+    
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+    
+    return 0;
+}
+
+// Tries to get the size of the terminal window with ioctl().
+// If this fails we will try a hack where we position the cursus at the bottom right and get the cursur position, this will provide us with the amount of rows (cursor report).
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+    
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+/*** append buffer ***/
+
+struct abuf {
+    char *b;
+    int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+// Use this instead of write to buffer the writing output
+void abAppend(struct abuf *ab, const char *s, int len) {
+    char *new = realloc(ab->b, ab->len + len);
+    if (new == NULL) return;
+    memcpy(&new[ab->len], s, len);
+    ab->b = new;
+    ab->len += len;
+}
+
+void abFree(struct abuf *ab) {
+    free(ab->b);
+}
+
+/*** output ***/
+
+// Draw a '~' at the beginning of each buffered row
+void editorDrawRows(struct abuf *ab) {
+    int y;
+    for (y = 0; y < E.screenrows; y++) {
+        abAppend(ab, "~", 1);
+        
+        if (y < E.screenrows - 1) { // To prevent a scroll at the end leaving the last line blank
+            abAppend(ab, "\r\n", 2);
+        }
+    }
+}
+
+void editorRefreshScreen() {
+    
+    struct abuf ab = ABUF_INIT;
+    
+    abAppend(&ab, "\x1b[?25l", 6);  // Hide cursor
+    abAppend(&ab, "\x1b[2J", 4);    // Escape character x1b + clear entire screen
+    abAppend(&ab, "\x1b[H", 3);     // Position cursor in top left corner. It positions the cursor in <esc>[x;yH, default is 1;1
+
+    editorDrawRows(&ab);
+    
+    abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25l", 6);
+
+    write(STDOUT_FILENO, ab.b, ab.len);
+    abFree(&ab);
+}
+
+/*** input ***/
+
+void editorProcessKeypress() {
+    char c = editorReadKey();
+    switch (c) {
+        case CTRL_KEY('q'):
+            // Clear and reposition cursor
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            
+            exit(0);
+            break;
+    }
+}
+
 /*** init ***/
+
+// Put the size of the terminal in the E structure.
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
     enableRawMode();
+    initEditor();
     
-    char c;
-    // read() returns the amount of read bytes, we store the read characters in the char c. We also exit upon the 'q'.
     while (1) {
-        char c = '\0';
-        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if (iscntrl(c)) {   // Checks for control characters like TAB
-            printf("%d\r\n", c);    // Carriage return \r and newline \n
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-        if (c == CTRL_KEY('q')) break;
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
+    
     return 0;
 }
